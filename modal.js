c56ee8b43711d79a502e7c84520e34db561f3d89
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-11-20 13:20:49 +0200

    2.0.1

--- modal.js
+++ modal.js
@@ -1,3 +1,12 @@
+document.querySelectorAll('[data-modal-toggle]').forEach((element) =>
+    element.addEventListener('click', ()=> 
+        Modal()
+    )
+)
+
+
+
+
 function Modal(){
 document.querySelector('.modal').classList.toggle("modal-on");
 document.querySelector('.wrapper').classList.toggle("wrapper-on")
