9c22dffe2d6b2ac9bb0014eaee1084ea273b98ed
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-11-22 16:36:18 +0200

    optimisation

--- modal.js
+++ modal.js
@@ -1,13 +1,19 @@
+//есть два варианта решения, через функцию, что загрезняет глобальную область видимости, но при разных слушателей ивента помогает оптимизироват. 
+//второй вариант по затратам памяти более оптимальный в решении именнно этой задачи. 
+
 document.querySelectorAll('[data-modal-toggle]').forEach((element) =>
-    element.addEventListener('click', ()=> 
-        Modal()
-    )
+    element.addEventListener('click', ()=> {
+        //Modal() 
+        document.querySelector('.modal').classList.toggle("modal-on");
+        document.querySelector('.wrapper').classList.toggle("wrapper-on")
+    })
 )
 
 
 
-
+/*
 function Modal(){
 document.querySelector('.modal').classList.toggle("modal-on");
 document.querySelector('.wrapper').classList.toggle("wrapper-on")
 }
+*/
